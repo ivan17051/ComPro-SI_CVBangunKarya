7ec2618a956302d75f839df71e38df4aee93e951
galihpribadi04 <galihpribadi04@gmail.com>
2020-07-28 21:20:13 +0700

    Tambahan database
    Database CRUD

---
+++ database/migrations/2020_07_28_130959_create_pemasukan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePemasukanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemasukan', function (Blueprint $table) {
+            $table->increments('id_pemasukan');
+            $table->date('tanggal_pemasukan');
+            $table->text('deskripsi');
+            $table->integer('jumlah_pemasukan_klien');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemasukan');
+    }
+}
---
+++ database/migrations/2020_07_28_141633_create_pengeluaran_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengeluaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengeluaran', function (Blueprint $table) {
+            $table->increments('id_pengeluaran');
+            $table->date('tanggal_belanja');
+            $table->string('list_belanja');
+            $table->text('deskripsi');
+            $table->integer('jumlah_barang');
+            $table->integer('harga_satuan');
+            $table->integer('jumlah');
+            $table->string('nama_toko');
+            $table->mediumText('upload_struk')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengeluaran');
+    }
+}
